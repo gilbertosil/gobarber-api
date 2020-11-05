da5b23272a25978574963d295bddf153ab91a91a
Gilberto Jr <junior.gilberto@gmail.com>
2020-11-04 23:04:37 -0300

    Inclusão do relacionamento dos models

--- src/database/migrations/1604457799912-CreateAppointments.ts
+++ src/database/migrations/1604457799912-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1604457799912 implements MigrationInterfa
           columns: [
             {
               name : 'id',
-              type: 'varchar',
+              type: 'uuid',
               isPrimary: true,
               generationStrategy: 'uuid',
               default: 'uuid_generate_v4()'
