a8a9ceceff34d15eea4362da152ac2af7e1335f3
Gilberto Jr <junior.gilberto@gmail.com>
2020-11-10 21:58:56 -0300

    Testes de agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentsService from './CreateAppointmentService';
 
@@ -15,5 +16,23 @@ describe('CreateAppointmentService', () => {
     expect(appointment.id).toBe('121212');
   });
 
-  it('should not be able to create two appointments on the same time', () => {});
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentsService(
+      fakeAppointmentRepository,
+    );
+    const appointmentDate = new Date(2020, 3, 23, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '121212',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '121212',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
